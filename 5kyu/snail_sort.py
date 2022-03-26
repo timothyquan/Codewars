a6abf9d496c946a098e8d574e997a686a95349bf
timothyquan <me@timquan.ca>
2022-03-26 11:45:34 +0700

    snail_sort: submitted, passed

--- 5kyu/snail_sort.py
+++ 5kyu/snail_sort.py
@@ -3,32 +3,27 @@ import numpy as np
 
 
 def snail(snail_map):
-    sides = [{'slice': np.s_[0, :], 'axis': 0},
-             {'slice': np.s_[:, -1], 'axis': 1},
-             {'slice': np.s_[-1, -1::-1], 'axis': 0},
-             {'slice': np.s_[-1::-1, 0], 'axis': 1}
+
+    sides = [{'slice': np.s_[0, :], 'axis': 0},  # top row
+             {'slice': np.s_[:, -1], 'axis': 1},  # right column downward
+             {'slice': np.s_[-1, -1::-1], 'axis': 0},  # bottom row
+             {'slice': np.s_[-1::-1, 0], 'axis': 1}  # left column upward
              ]
 
     newarray = np.array(array)
 
-    print(newarray)
-    print('======Start: ')
     snailsorted = []
     while len(newarray) > 0:
         for side in sides:
             snailsorted = snailsorted + list(newarray[side['slice']])
-            print("To append: ", newarray[side['slice']])
             newarray = np.delete(
                 newarray, side['slice'][side['axis']], side['axis'])
-            print(newarray)
-            print('======')
             if len(newarray) < 1:
                 break
     return(snailsorted)
 
 
 if __name__ == '__main__':
-
     array = [[1, 2, 3],  # test 1
              [4, 5, 6],
              [7, 8, 9]]
@@ -37,7 +32,15 @@ if __name__ == '__main__':
              [8, 9, 4],
              [7, 6, 5]]
 
+    array = [[1, 2, 3, 4],  # test 3
+             [5, 6, 7, 8],
+             [9, 10, 11, 12],
+             [13, 14, 15, 16]]
+
     snailsorted = snail(array)
+
     print(snailsorted)
     # assert snailsorted == [1, 2, 3, 6, 9, 8, 7, 4, 5] #test 1
-    assert snailsorted == [1, 2, 3, 4, 5, 6, 7, 8, 9]  # test 2
+    # assert snailsorted == [1, 2, 3, 4, 5, 6, 7, 8, 9]  # test 2
+    assert snailsorted == [1, 2, 3, 4, 8, 12, 16,
+                           15, 14, 13, 9, 5, 6, 7, 11, 10]  # test 3
